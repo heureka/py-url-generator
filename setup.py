bd2827f320384a9045ab8e177918a45d15f63a74
Vladimír Kašpar <vladimir.kaspar@heureka.cz>
2018-06-27 08:51:39 +0200

    Update the name of library

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='py-url-generator',
+    name='url-generator',
     description="Universal URL generator.",
     version='1.0.0',
     license='Apache',
