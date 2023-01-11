732357b41938c80be4ff4f67c85f3fd6227062fe
David Šanda <sanda.david@gmail.com>
2023-01-11 13:13:42 +0100

    add readme as description to pypi.org

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 setup(
     name='url-generator',
     description="Universal URL generator.",
-    version='1.1.0',
+    version='1.1.1',
     license='Apache',
     author='Heureka.cz',
     author_email='vyvoj@heureka.cz',
     url='https://github.com/heureka/py-url-generator',
-    packages=['url_generator']
+    packages=['url_generator'],
+    long_description=open('./README.md', 'r').read(),
+    long_description_content_type='text/markdown',
 )
